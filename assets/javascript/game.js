1792b2335672a0576db4b2912203aa528af0e554
Christopher McKee <chris.mckee.13@gmail.com>
2019-10-10 20:31:19 -0700

    adds new game functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,28 +1,42 @@
 const htmlNode = document.getElementById("game");
 const alphabet = "qwertyuiopasdfghjklzxcvbnm"
+const GUESSTOTAL = 10
 
 var game = {
+    inProgress: true,
     wins: 0,
     losses: 0,
-    remainingGuesses: 10,
-    wordProgress: ["_", "_", "_", "_"],
+    remainingGuesses: GUESSTOTAL,
+    wordProgress: ["-", "-", "-", "-"],
     actualWord: ["j", "a", "z", "z"],
     wrongGuesses: [],
+    words: ["handsomely", "truculent", "glass", "haircut", "careless", "line", "develop", "sound", "splendid", "accessible", "van", "synonymous"],
+    wordIndex: 0,
 
     guessLetter: function (letter) {
 
         if (this.actualWord.indexOf(letter) === -1) {
             if (this.wrongGuesses.indexOf(letter) === -1) {
+                // Wrong Guess
                 this.wrongGuesses.push(letter);
+                this.wrongGuesses.sort();
+                //sort
+                //split("")
+                //join("")
                 this.remainingGuesses += -1
                 if (this.remainingGuesses === 0) {
                     //game over code here
+                    this.losses++;
                     htmlNode.innerHTML = "<h2>Word: " + this.wordProgress.join("") + "</h2>";
                     htmlNode.innerHTML += "<h2>Guessed Letters: " + this.wrongGuesses + "</h2>";
                     htmlNode.innerHTML += "<h2>Reamining Guesses: " + this.remainingGuesses + "</h2>";
                     htmlNode.innerHTML += "<h2>Wins: " + this.wins + "</h2>";
                     htmlNode.innerHTML += "<h2>Losses: " + this.losses + "</h2>";
                     htmlNode.innerHTML += "<h2>Game Over</h2>";
+                    htmlNode.innerHTML += "<h3>Press any key to continue</h3>";
+                    this.inProgress = false;
+
+
                 } else {
                     //game continues
                     htmlNode.innerHTML = "<h2>Word: " + this.wordProgress.join("") + "</h2>";
@@ -42,15 +56,18 @@ var game = {
                 }
 
             }
-            if (this.wordProgress.indexOf("_") === -1) {
+            if (this.wordProgress.indexOf("-") === -1) {
                 //console.log("ping2");
                 //win
+                this.wins++;
                 htmlNode.innerHTML = "<h2>Word: " + this.wordProgress.join("") + "</h2>";
                 htmlNode.innerHTML += "<h2>Guessed Letters: " + this.wrongGuesses + "</h2>";
                 htmlNode.innerHTML += "<h2>Reamining Guesses: " + this.remainingGuesses + "</h2>";
                 htmlNode.innerHTML += "<h2>Wins: " + this.wins + "</h2>";
                 htmlNode.innerHTML += "<h2>Losses: " + this.losses + "</h2>";
                 htmlNode.innerHTML += "<h2>Win!</h2>";
+                htmlNode.innerHTML += "<h3>Press any key to continue</h3>";
+                this.inProgress = false;
             } else {
                 //console.log("ping3");
                 //game continues
@@ -64,6 +81,22 @@ var game = {
 
 
     },
+    newGame: function () {
+        this.remainingGuesses = GUESSTOTAL;
+        this.wrongGuesses = [];
+        this.actualWord = this.words[this.wordIndex].split("");
+        this.wordIndex++;
+        this.wordProgress = Array(this.actualWord.length);
+        for (let i = 0; i < this.wordProgress.length; i++) {
+            this.wordProgress[i] = "-";
+        }
+        this.inProgress = true;
+        htmlNode.innerHTML = "<h2>Word: " + this.wordProgress.join("") + "</h2>";
+        htmlNode.innerHTML += "<h2>Guessed Letters: " + this.wrongGuesses + "</h2>";
+        htmlNode.innerHTML += "<h2>Reamining Guesses: " + this.remainingGuesses + "</h2>";
+        htmlNode.innerHTML += "<h2>Wins: " + this.wins + "</h2>";
+        htmlNode.innerHTML += "<h2>Losses: " + this.losses + "</h2>";
+    },
 
 };
 
@@ -76,8 +109,10 @@ htmlNode.innerHTML += "<h2>Losses: " + game.losses + "</h2>";
 
 document.onkeyup = function (event) {
     const inputKey = event.key;
-    if (alphabet.indexOf(inputKey) !== -1) {
+    if (alphabet.indexOf(inputKey) !== -1 & game.inProgress) {
         game.guessLetter(inputKey);
+    } else if (!game.inProgress) {
+        game.newGame();
     }
 };
 
